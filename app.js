e1a81c4211fcafbdb2761fed2625348c216ce5e8
NayanTripathi <20bcs143@iiitdmj.ac.in>
2021-11-13 14:46:38 +0530

    port to ejs,change app.js for ejs

--- app.js
+++ app.js
@@ -2,29 +2,32 @@ const express = require(`express`);
 const app = express();
 const path = require(`path`);
 const https = require(`https`);
-const port = process.env.PORT || 3000;
+const ejs = require(`ejs`);
+const port = process.env.PORT || 4000;
+const fs = require(`fs`);
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const { create } = require('domain');
 
+app.set(`view engine`,`ejs`);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, `public`)));
 
 app.get(`/login`, (req, res) => {
-    res.sendFile(path.join(__dirname, `login.html`));
+    res.render(`login`);
 });
 app.get(`/about`, (req, res) => {
-    res.sendFile(path.join(__dirname, `about.html`));
+    res.render(`about`);
 });
 app.get(`/create_user`, (req, res) => {
-    res.sendFile(path.join(__dirname, `create_acc_user.html`));
+    res.render(`create_acc_user`);
 });
 app.get(`/create_driver`, (req, res) => {
-    res.sendFile(path.join(__dirname, `create_acc_driver.html`));
+    res.render(`create_acc_driver`);
 });
 app.get(`/booking`,(req,res)=>{
-    res.sendFile(path.join(__dirname,`booking.html`));
+        res.render(`booking`);
 });
 app.post(`/login_check`, (req, res) => {
     if (req.body.userType == "user") {
@@ -95,31 +98,10 @@ app.post(`/create_user_acc`, (req, res) => {
         query = mysql.format(query, insert);
         conn.query(query, (err, result, fields) => {
             if (err) {
-                res.write(`
-                <!DOCTYPE html>
-                        <html>          
-                        <head>
-                            <title>Failure</title>
-                            <!-- Bootstrap and CSS -->
-                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-                                integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-                                integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"
-                                defer></script>
-                        </head>
-                        <body>
-                        <main class="container py-4 mt-4" >
-                        <article class="h-50 p-5 text-white bg-dark border rounded-3">
-                            <h2 class="text-danger display-1 fw-normal">Failure</h2>
-                            <p class="col-md-8 fs-4 ps-1">Data insertion failure!<br />Error code: ${err}</p>
-                            <a class="btn btn-outline-light btn-lg ms-1" href="/create_user">Go Back</a>
-                        </article>
-                        </main>
-                        </body> `);
-                        res.end();
+                res.render(`failure_user`,{error:err});
             }
             else{
-                res.sendFile(path.join(__dirname,`success_user.html`));
+                res.render(`success_user`);
             }
         });
     });
@@ -139,36 +121,18 @@ app.post(`/create_driver_acc`, (req, res) => {
         query = mysql.format(query, insert);
         conn.query(query, (err, result, fields) => {
             if (err) {
-                res.write(`
-                <!DOCTYPE html>
-                        <html>          
-                        <head>
-                            <title>Failure</title>
-                            <!-- Bootstrap and CSS -->
-                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-                                integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-                                integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"
-                                defer></script>
-                        </head>
-                        <body>
-                        <main class="container py-4 mt-4" >
-                        <article class="h-50 p-5 text-white bg-dark border rounded-3">
-                            <h2 class="text-danger display-1 fw-normal">Failure</h2>
-                            <p class="col-md-8 fs-4 ps-1">Data insertion failure!<br />Error code: ${err}</p>
-                            <a class="btn btn-outline-light btn-lg ms-1" href="/create_driver">Go Back</a>
-                        </article>
-                        </main>
-                        </body> `);
-                        res.end();
+                res.render(`failure_driver`,{error:err});
             }
             else{
-                res.sendFile(path.join(__dirname,`success_driver.html`));
+                res.render(`success_driver`);
             }
         });
     });
 });
 
+app.post(`/available_buses`,(req,res)=>{
+    
+});
 
 app.listen(port, () => {
     console.log(`Server runnning at port ${port}`);
